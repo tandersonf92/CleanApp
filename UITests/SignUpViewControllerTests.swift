28f8426605a00c3ffdfdb88e7cf6a55bc880ee45
tandersonf <anderson.oliveira@cora.com.br>
2024-03-04 08:25:08 -0300

    add UI layer

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by Cora on 04/03/24.
+//
+
+import XCTest
+@testable import UI
+
+final class SignUpViewControllerTests: XCTestCase {
+
+    func test_() {
+
+    }
+
+}
