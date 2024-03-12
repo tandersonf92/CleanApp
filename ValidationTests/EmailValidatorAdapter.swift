7444dd5b6826697e6e952bf05424b475d263f98e
tandersonf <anderson.oliveira@cora.com.br>
2024-03-12 08:18:29 -0300

    move EmailValidatorAdapter to its own file

--- ValidationTests/EmailValidatorAdapter.swift
+++ ValidationTests/EmailValidatorAdapter.swift
@@ -3,17 +3,6 @@ import XCTest
 
 @testable import Validation
 
-public final class EmailValidatorAdapter: EmailValidator {
-    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-    public func isValid(email: String) -> Bool {
-        let range = NSRange(location: 0, length: email.utf16.count)
-        if let regex = try? NSRegularExpression(pattern: pattern) {
-            return regex.firstMatch(in: email, range: range) != nil
-        }
-        return false
-    }
-}
-
 final class EmailValidatorAdapterTests: XCTestCase {
 
     func test_InvalidEmails() {
