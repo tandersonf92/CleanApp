7be4e71a436818fe8b7aa4a1acd85e267a51f5e9
tandersonf <anderson.oliveira@cora.com.br>
2024-03-14 09:24:19 -0300

    MainQueueDispatchDecorator to its own file

---
+++ Main/Decorators/File.swift
@@ -0,0 +1,26 @@
+import Domain
+import Foundation
+
+public final class MainQueueDispatchDecorator<T> {
+
+    private let instance: T
+
+    public init(_ instance: T) {
+        self.instance = instance
+    }
+
+    func dispatch(completion: @escaping() -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { completion() }
+        }
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: AddAccountUseCase where T: AddAccountUseCase {
+    public func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
+        instance.add(addAccountModel: addAccountModel) { [weak self] result in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
--- Main/Factories/UseCaseFactory.swift
+++ Main/Factories/UseCaseFactory.swift
@@ -18,27 +18,3 @@ enum UseCaseFactory {
         return MainQueueDispatchDecorator(remoteAddAccount)
     }
 }
-
-public final class MainQueueDispatchDecorator<T> {
-
-    private let instance: T
-
-    public init(_ instance: T) {
-        self.instance = instance
-    }
-
-    func dispatch(completion: @escaping() -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async { completion() }
-        }
-        completion()
-    }
-}
-
-extension MainQueueDispatchDecorator: AddAccountUseCase where T: AddAccountUseCase {
-    public func add(addAccountModel: Domain.AddAccountModel, completion: @escaping (Result<AccountModel, DomainError>) -> Void) {
-        instance.add(addAccountModel: addAccountModel) { [weak self] result in
-            self?.dispatch { completion(result) }
-        }
-    }
-}
