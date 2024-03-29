d8d048fa219a455a60103ce9379a45999ec6fb65
tandersonf <anderson.oliveira@cora.com.br>
2024-03-29 19:42:20 -0300

    change AccountModel to have only an accessToken

--- DataTests/Helpers/AccountModelFactory.swift
+++ DataTests/Helpers/AccountModelFactory.swift
@@ -1,10 +1,7 @@
 import Domain
 
 func makeAccountModel() -> AccountModel {
-    AccountModel(id: "any_id",
-                 name: "any_name",
-                 email: "any_email@email.com",
-                 password: "any_password")
+    AccountModel(accessToken: "any_token")
 }
 
 func makeAddAccountModel() -> AddAccountModel {
