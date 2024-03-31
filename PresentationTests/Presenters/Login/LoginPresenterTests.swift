cbb89c0e664c697dc711f5bfb1b45e65ef757fbe
tandersonf <anderson.oliveira@cora.com.br>
2024-03-31 19:04:15 -0300

    ensure LoginPresenter showes error message if Validation Fails

--- PresentationTests/Presenters/Login/LoginPresenterTests.swift
+++ PresentationTests/Presenters/Login/LoginPresenterTests.swift
@@ -12,12 +12,27 @@ final class LoginPresenterTests: XCTestCase {
         sut.login(viewModel: viewModel)
         XCTAssertTrue(NSDictionary(dictionary: validationSpy.data!).isEqual(to: viewModel.toJson()!))
     }
+
+    func test_Login_ShouldShowErrorMessageIfValidationFails() {
+        let alertViewSpy = AlertViewSpy()
+        let validationSpy = ValidationSpy()
+        let sut = makeSut(alertView: alertViewSpy, validation: validationSpy)
+
+        let exp = expectation(description: "waiting")
+        alertViewSpy.observe { viewModel in
+            XCTAssertEqual(viewModel, AlertViewModel(title: "Falha na validação", message: "Erro"))
+            exp.fulfill()
+        }
+        validationSpy.simulateError()
+        sut.login(viewModel: makeLoginViewModel())
+        wait(for: [exp], timeout: 1)
+    }
 }
 
 extension LoginPresenterTests {
-    func makeSut(validation: ValidationSpy = ValidationSpy(), file: StaticString = #filePath,
+    func makeSut(alertView: AlertViewSpy = AlertViewSpy(), validation: ValidationSpy = ValidationSpy(), file: StaticString = #filePath,
                  line: UInt = #line) -> LoginPresenter {
-        let sut = LoginPresenter(validation: validation)
+        let sut = LoginPresenter(validation: validation, alertView: alertView)
         checkMemoryLeak(for: sut, file: file, line: line)
         return sut
     }
